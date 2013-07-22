cfc155046a0e5753c5c063deed2285a5e16decbb
Sriram Velamur <sriram.m@imaginea.com>
2013-07-22 20:31:17 +0530

    Added main.
    TODO: Update creds flow usage as earlier

---
+++ main.py
@@ -0,0 +1,44 @@
+# coding=utf-8
+"""Github social"""
+
+__author__ = 'sriramm'
+
+import sys
+sys.dont_write_bytecode = True
+
+from GitHubAccess import GitHub
+from copy import deepcopy
+
+
+def main():
+    """Main caller method"""
+    github_obj = GitHub(creds=(None, None),
+                        config={
+                            'reverse': True,
+                            'auth': True,
+                            'safe_json': True
+                        })
+    repos = github_obj.search_repos({
+        'keyword': 'shell',
+        'sort': 'followers',
+        'order': 'desc'
+    }, returns=True).get('repositories', [])
+    owners = {}
+    for repo in repos:
+        owner = repo.get('owner')
+        if owner in owners.keys():
+            owners[owner] += 1
+        else:
+            owners[owner] = 1
+
+    owners_dup = deepcopy(owners)
+    for (key, val) in owners.iteritems():
+        if val < 2:
+            owners_dup.pop(key)
+
+    for (username, repo_count) in owners_dup.iteritems():
+        print username, len(github_obj.get_user_info(username, action='repos', returns=True))
+
+
+if __name__ == '__main__':
+    main()
