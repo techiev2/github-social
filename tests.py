8570fc0f2ff21ea9672589c0f27dcac4da7eb8f7
Sriram Velamur <sriram.m@imaginea.com>
2013-07-24 12:22:06 +0530

    Pylint/PEP compliance fixtures

--- tests.py
+++ tests.py
@@ -11,17 +11,22 @@ from main import get_auth
 
 
 class BaseTests(unittest.TestCase):
+    """
+    Base set of tests for GitHubAccess module's GitHub class.
+    setUp sets up an instance of GitHub access and provides
+    access to instance methods.
+    """
     def setUp(self):
         """Base tests setup. Initialize a github object instance."""
         self.response = None
-        self.creds = get_auth(returns=True)
+        self.creds = get_auth()
         self.github_obj = GitHub(creds=self.creds['creds'],
-                        config={
-                            'reverse': False,
-                            'auth': True,
-                            'safe_json': True,
-                            'client_data': self.creds['client']
-                        })
+                                 config={
+                                     'reverse': False,
+                                     'auth': True,
+                                     'safe_json': True,
+                                     'client_data': self.creds['client']
+                                 })
 
     # def testGetData(self):
     #     """Test get data method. Must return <dict>"""
@@ -31,9 +36,9 @@ class BaseTests(unittest.TestCase):
     #     assert self.response.get('public_repos', None) is not None
     #     self.assertIsInstance(self.response, dict)
     #
-    def testAuth(self):
+    def test_auth_flow(self):
         """Test private session authorize method"""
-        self.github_obj._auth_session()
+        self.github_obj.__getattribute__('_auth_session')()
         self.assertIsInstance(self.github_obj.response, dict)
 
 
