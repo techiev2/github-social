a0ac2aca91d13941b589a4e124fae1f4d2d61a7f
Sriram Velamur <sriram.m@imaginea.com>
2013-07-24 19:01:08 +0530

    Added basic get_auth method
    Could be used in main or replaced with custom auth loader

--- GitHubAccess/__init__.py
+++ GitHubAccess/__init__.py
@@ -16,10 +16,70 @@ from requests.auth import HTTPBasicAuth
 import json
 import logging
 from copy import deepcopy
+import argparse
+import getpass
 
 # Boolean checklist. Could be a case for ast ?
 TRUE = [True, 1, '1', 'True']
 FALSE = [False, [], None, 0, '0', 'False', 'None']
+
+
+def get_auth(returns=True):
+    """
+    Get authentication data
+    :param returns:bool Boolean check to return data from get_auth
+    """
+    (creds, client) = (None, None)
+    user_creds = {}
+    arg_parser = argparse.ArgumentParser(description="Creds loader")
+    arg_parser.add_argument("--creds", help="Credentials file name")
+    creds_file = arg_parser.parse_args().__getattribute__('creds')
+    no_creds = "No credentials file provided. Defaulting to shell input"
+    uname_input = "Please enter your GitHub username to authenticate: "
+    upass_input = "Please enter your GitHub password to authenticate: "
+    client_id_input = "Please enter your GitHub client ID: "
+    client_secret_input = "Please enter your GitHub client secret: "
+    if not creds_file:
+        print no_creds
+        try:
+            uname = raw_input(uname_input)
+            upass = getpass.getpass(upass_input)
+            client_id = raw_input(client_id_input) or ''
+            client_secret = raw_input(client_secret_input) or ''
+        except KeyboardInterrupt:
+            quit("\nKeyboard interrupt. Exiting")
+
+        if uname and upass:
+            creds = (uname, upass)
+
+        client = {
+            'client_id': client_id,
+            'client_secret': client_secret
+        }
+
+    else:
+        #TODO: Fix a loadable json config loader.
+        with open(creds_file, "r") as cfile:
+            creds = cfile.read()
+
+        try:
+            data = json.loads(creds)
+            creds = (data.get('uname', None), data.get('upass', None))
+            client = {
+                'client_id': data.get('client_id', ''),
+                'client_secret': data.get('client_secret', '')
+            }
+        except TypeError:
+            pass
+
+    if not creds:
+        raise Exception("No credentials found. Exiting")
+
+    user_creds['creds'] = creds
+    user_creds['client'] = client
+
+    if returns:
+        return user_creds
 
 
 class GitHub(object):
@@ -246,5 +306,20 @@ class GitHub(object):
         response = self._get_data(url, returns=returns)
         if returns:
             return response
+
+    def get_user_events(self, user_name=None, returns=False):
+        """
+        Get public events initiated by specified user.
+        :param user_name:str Username to query against.
+        :param returns:bool Boolean param to toggle return for testing.
+                    Method returns response if True;
+                    else updates response class member
+        """
+        if not (user_name and isinstance(user_name, str)):
+            raise Exception("No valid username provided. Exiting")
+        base_url =  "{0}/users/{1}/events/public".format(self.base_url, user_name)
+        self.response = self._get_data(base_url, 'get', {}, True)
+        if returns:
+            return self.response
 
 __all__ = ['GitHub']
